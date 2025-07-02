52488b867ef268d2559d46c3a71edf8cd715db94
ayman <danishaiman3b@gmail.com>
2025-07-02 16:44:13 +0800

    Cleaning :D

--- sql/profiles.sql
+++ sql/profiles.sql
@@ -1,36 +1,36 @@
 create table
-  public.profiles (
+    public.profiles (
     id uuid not null references auth.users on delete cascade,
     full_name text,
     email text unique,
     avatar_url text,
     primary key (id)
-  );
+    );
 
 alter table profiles enable row level security;
 
 create policy "Public profiles are viewable by everyone." on profiles for
 select
-  using (true);
+    using (true);
 
 create policy "Users can insert their own profile." on profiles for insert
 with
-  check (auth.uid () = id);
+    check (auth.uid () = id);
 
 create policy "Users can update own profile." on profiles
 for update
-  using (auth.uid () = id);
+    using (auth.uid () = id);
 
 create function public.handle_new_user () returns trigger as $$
 begin
-  insert into public.profiles (id, full_name, avatar_url, email)
-  values (
+    insert into public.profiles (id, full_name, avatar_url, email)
+    values (
     new.id, 
     new.raw_user_meta_data->>'full_name', 
     new.raw_user_meta_data->>'avatar_url',
     new.raw_user_meta_data->>'email'
     );
-  return new;
+    return new;
 end;
 $$ language plpgsql security definer;
 
