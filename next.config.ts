92644b3c719a5cf97e7e6711c3e840cee3802eaf
ayman <danishaiman3b@gmail.com>
2025-07-03 11:00:55 +0800

    Refactor image configuration to use remotePatterns for external image sources

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.rawg.io',
+        pathname: '**',
+      },
+    ],
   },
 };
 
