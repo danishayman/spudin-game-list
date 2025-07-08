29c85073b10ae6c733b209ad203c703a32dcac96
ayman <danishaiman3b@gmail.com>
2025-07-08 16:28:48 +0800

    Implement skeleton ui for game details

--- app/games/[id]/layout.tsx
+++ app/games/[id]/layout.tsx
@@ -11,7 +11,7 @@ export default function GameDetailsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-slate-900">
+    <div className="min-h-screen bg-slate-900 relative">
       {children}
     </div>
   );
